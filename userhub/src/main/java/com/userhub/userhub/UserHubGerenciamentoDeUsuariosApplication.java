3ffb45e6860d1da0b84a1e9e21c8b528b05bb93a
Elton Almeida <72717766+Elton-mao@users.noreply.github.com>
2023-11-28 16:33:18 -0300

    start project

---
+++ userhub/src/main/java/com/userhub/userhub/UserHubGerenciamentoDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.userhub.userhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHubGerenciamentoDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHubGerenciamentoDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ userhub/src/test/java/com/userhub/userhub/UserHubGerenciamentoDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userhub.userhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHubGerenciamentoDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
